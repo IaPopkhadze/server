d8b33823cd738aaf8c8b3b87c20e4d97886ac0d0
IaPopkhadze <97228711+IaPopkhadze@users.noreply.github.com>
2023-07-27 00:01:11 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.use(
     origin: ["http://192.168.1.5:3000"],
   })
 );
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("/");
@@ -24,6 +26,15 @@ const getPersons = (req, res) => {
   });
 };
 
+const updateDeletePersons = (req, res) => {
+  const persons = req;
+  console.log(req.body);
+  // fs.writeFile("presons.json", persons, (err) => console.log(err));
+
+  res.send("ok");
+};
+
 app.get("/persons", getPersons);
+app.post("/persons", updateDeletePersons);
 
 app.listen(port, console.log("Server is running on port:", port));
